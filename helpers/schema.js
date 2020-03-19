bf98277bd37fccf85673658fc618dd63ec419a11
Derek S <dschwan21@me.com>
2020-03-18 22:17:44 -0700

    started building out schema

--- helpers/schema.js
+++ helpers/schema.js
@@ -1,1 +1,11 @@
 // this will be where graphql backend will be built
+const express = require("express"); // importing a CommonJS module
+const expressGraphQL = require("express-graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull
+} = require("graphql");
